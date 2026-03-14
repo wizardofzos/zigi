b578dc42df403b37ba6518b0cf00c3f24b14d180
Wizard of z/OS <wizard@zdevops.com>
2026-03-15 00:03:50 +0100

    Path + str lol

--- .github/scripts/generate_xmit.py
+++ .github/scripts/generate_xmit.py
@@ -4,6 +4,7 @@ import sys
 from pathlib import Path 
 from xmi import create_xmi
 import shutil
+import posixpath
 
 # We ZIGI.INSTALL.PDS (as an XMIT)
 
@@ -19,10 +20,10 @@ outputfile = Path('ZIGI.INSTALL.PDS')
 
 tmppds = Path(sys.argv[1])
 
-shutil.copy(lic, tmppds + Path("/GPLLIC"))
-shutil.copy(readme,tmppds + Path("/$README"))
-shutil.copy(release,tmppds + Path("/RELEASE"))
-shutil.copy(install,tmppds + Path("/$INSTALL"))
+shutil.copy(lic, posixpath.join(tmppds,"/GPLLIC"))
+shutil.copy(readme, posixpath.join(tmppds,"/$README"))
+shutil.copy(release, posixpath.join(tmppds,"/RELEASE"))
+shutil.copy(install, posixpath.join(tmppds,"/$INSTALL"))
 
 create_xmi(execs, output_file=tmppds + "/EXEC")
 create_xmi(panels, output_file=tmppds + "/PANELS")
